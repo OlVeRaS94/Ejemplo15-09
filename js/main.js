94d60dd21f488be67f179ae845a4dff8a1867a9c
Toni <juniortoni16@gmail.com>
2022-09-15 13:29:29 +0200

    Ejemplos de ejercicios que hemos hecho el dia 15 de septiembre Js

---
+++ js/main.js
@@ -0,0 +1,108 @@
+//let nota = parseInt(prompt("Dime que nota has sacado:"));
+
+//if(nota >=0 && nota <= 4){
+   // console.log("insuficiente");
+//}else if(nota >=5 && nota <=6){
+   // console.log("Suficiente");
+//}else if(nota >=7 && nota <=8){
+   // console.log("Notable");
+//}else if(nota >=9 && nota <=10){
+    //console.log("Sobresaliente");
+//};
+
+
+// switch (nota) {
+
+//     case 0:
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//         console.log("Insuficiente");
+//     break;
+
+//     case 5:
+//     case 6:
+//         console.log("Suficiente");
+//     break;
+
+//     case 7:
+//     case 8:
+//         console.log("Notable");
+//     break;
+
+//     case 9:
+//     case 10:
+//         console.log("Excelente");
+//     break;
+
+//     default:
+//         console.log("No se que has puesto, pero estás suspendido");
+//     break;
+// }
+
+
+// let edad = 17;
+
+// let pelicula = "Jaws";
+
+// if(edad < 18){
+//     console.log("No puedes ver Jaws");
+// }else{
+//     console.log("Si puedes ver Jaws");
+// };
+
+//Mientras tanto, en el mundo de las ternarias.....
+
+// let edad = 17;
+
+// let verPelicula = edad < 18 ? "No puedes ver Jaws" : "Si puedes ver Jaws"
+
+// console.log(verPelicula);
+
+
+
+// for(let i = 1; i <= 100; i++){
+
+//     console.log("Hola David, eres un genio");
+// }
+
+//let vida = -5;
+
+
+// while(vida >= 0){
+
+//     vida -= 5;
+//     console.log(vida);
+
+//     if(vida == 0){
+//         break;
+//     }
+
+// }
+
+// do {
+
+//     vida -= 5;
+//     console.log(vida);
+
+//     if(vida == 0){
+//         break;
+//     }
+
+// } while (vida >= 0);
+
+//let numero = parseInt(prompt("dime un numero:"));
+
+//let contador = 1;
+
+//while(contador <= 10){
+
+ //   console.log(numero, "x", contador,"=", (numero*contador));
+  //  contador++;
+//};
+
+for(let i = 1; i <= 10; i++){
+
+    console.log(numero, " X ", i, " = ", numero*i);
+};
